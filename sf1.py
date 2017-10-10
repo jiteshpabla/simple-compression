5c1a355625dcb7ba684dd2011da6409876199021
Jitesh Pabla <jiteshpabla97@gmail.com>
2017-10-10 12:40:58 +0530

    updated sf1.py

--- sf1.py
+++ sf1.py
@@ -1,12 +1,16 @@
-# Shannon-Fano Data Compression
-# http://en.wikipedia.org/wiki/Shannon%E2%80%93Fano_coding
-# (Max compressible file size: 2**32 bytes)
-# FB - 201012153
+
 import sys
 import os
 
-def shannon_fano_encoder(iA, iB): # iA to iB : index interval
-    global tupleList
+#filename=''
+
+bitStream = '' #109
+#byteArr = []
+bitPosition = 0 #144
+
+
+def shannon_fano_encoder(iA, iB, tupleList): # iA to iB : index interval
+    #global tupleList
     size = iB - iA + 1
     if size > 1:
         # Divide the list into 2 groups.
@@ -19,8 +23,8 @@ def shannon_fano_encoder(iA, iB): # iA to iB : index interval
             else: # bottom group
                 tupleList[i] = (tup[0], tup[1], tup[2] + '1')
         # do recursive calls for both groups
-        shannon_fano_encoder(iA, mid - 1)
-        shannon_fano_encoder(mid, iB)
+        shannon_fano_encoder(iA, mid - 1, tupleList)
+        shannon_fano_encoder(mid, iB, tupleList)
 
 def byteWriter(bitStr, outputFile):
     global bitStream
@@ -30,8 +34,8 @@ def byteWriter(bitStr, outputFile):
         bitStream = bitStream[8:]
         outputFile.write(chr(int(byteStr, 2)))
 
-def bitReader(n): # number of bits to read
-    global byteArr
+def bitReader(n, byteArr): # number of bits to read
+    #global byteArr
     global bitPosition
     bitStr = ''
     for i in range(n):
@@ -43,25 +47,33 @@ def bitReader(n): # number of bits to read
         bitPosition += 1 # prepare to read the next bit
     return bitStr
 
+'''
 # MAIN
 if len(sys.argv) != 4:
     print 'Usage: ShannonFano.py [e|d] [path]InputFileName [path]OutputFileName'
     sys.exit()
 mode = sys.argv[1] # encoding/decoding
-inputFile = sys.argv[2]
-outputFile = sys.argv[3]
+'''
 
-# read the whole input file into a byte array
-fileSize = os.path.getsize(inputFile)
-fi = open(inputFile, 'rb')
-# byteArr = map(ord, fi.read(fileSize))
-byteArr = bytearray(fi.read(fileSize))
-fi.close()
-fileSize = len(byteArr)
-print 'File size in bytes:', fileSize
-print
+def shannon_compress(filename):
 
-if mode == 'e': # FILE ENCODING
+    print 'shannon compression running'
+
+    inputFile = filename
+    outputFile = filename+'_shannon'
+
+    # read the whole input file into a byte array
+    fileSize = os.path.getsize(inputFile)
+    fi = open(inputFile, 'rb')
+    # byteArr = map(ord, fi.read(fileSize))
+    #global byteArr
+    byteArr = bytearray(fi.read(fileSize))
+    fi.close()
+    fileSize = len(byteArr)
+    print 'File size in bytes:', fileSize
+    print
+
+    #if mode == 'e': # FILE ENCODING
     # calculate the total number of each byte value in the file
     freqList = [0] * 256
     for b in byteArr:
@@ -76,7 +88,8 @@ if mode == 'e': # FILE ENCODING
     # sort the list according to the frequencies descending
     tupleList = sorted(tupleList, key=lambda tup: tup[0], reverse = True)
 
-    shannon_fano_encoder(0, len(tupleList) - 1)
+    #shannon_fano_encoder(0, len(tupleList) - 1)
+    shannon_fano_encoder(0, len(tupleList) - 1, tupleList)
     # print 'The list of (frequency, byteValue, encodingBitStr) tuples:'
     # print tupleList
     # print
@@ -84,12 +97,11 @@ if mode == 'e': # FILE ENCODING
     # create a dictionary of byteValue : encodingBitStr pairs
     dic = dict([(tup[1], tup[2]) for tup in tupleList])
     del tupleList # unneeded anymore
-    # print 'The dictionary of byteValue : encodingBitStr pairs:'
     # print dic
 
     # write a list of (byteValue,3-bit(len(encodingBitStr)-1),encodingBitStr)
     # tuples as the compressed file header
-    bitStream = ''
+    #bitStream = ''
     fo = open(outputFile, 'wb')
     fo.write(chr(len(dic) - 1)) # first write the number of encoding tuples
     for (byteValue, encodingBitStr) in dic.iteritems():
@@ -116,28 +128,59 @@ if mode == 'e': # FILE ENCODING
     for b in byteArr:
         byteWriter(dic[b], fo)
 
-    byteWriter('0' * 8, fo) # to write the last remaining bits (if any)
+    byteWriter('0' * 8, fo) #write the last remaining bits
     fo.close()
 
-elif mode == 'd': # FILE DECODING
-    bitPosition = 0
-    n = int(bitReader(8), 2) + 1 # first read the number of encoding tuples
+    #----------------COMPRESSION RATIO------------------
+
+    fileSize2 = os.path.getsize(outputFile)
+    print 'File size in bytes (output):', fileSize2
+
+    compression_ratio = float(fileSize2)/float(fileSize)
+    return compression_ratio
+
+
+
+
+
+def shannon_decompress(filename):
+
+    print 'shannon decompression running'
+
+    inputFile = filename
+    #outputFile = filename[:-8]
+    #outputFile = filename[:-8]+'decomp.txt'
+    outputFile = filename[:filename.index('_shannon')]
+    outputFile = outputFile[:outputFile.index('.')] + 'Decompressed' + outputFile[outputFile.index('.'):]	
+        # read the whole input file into a byte array
+    fileSize = os.path.getsize(inputFile)
+    fi = open(inputFile, 'rb')
+    # byteArr = map(ord, fi.read(fileSize))
+    #global byteArr
+    byteArr = bytearray(fi.read(fileSize))
+    fi.close()
+    fileSize = len(byteArr)
+    print 'File size in bytes:', fileSize
+    print
+
+    #bitPosition = 0
+    n = int(bitReader(8,byteArr), 2) + 1 # first read the number of encoding tuples
     # print 'Number of encoding tuples:', n
     dic = dict()
     for i in range(n):
         # read the byteValue
-        byteValue = int(bitReader(8), 2)
+        byteValue = int(bitReader(8,byteArr), 2)
         # read 3-bit(len(encodingBitStr)-1) value
-        m = int(bitReader(3), 2) + 1
+        m = int(bitReader(3,byteArr), 2) + 1
         # read encodingBitStr
-        encodingBitStr = bitReader(m)
+        encodingBitStr = bitReader(m,byteArr)
         dic[encodingBitStr] = byteValue # add to the dictionary
     # print 'The dictionary of encodingBitStr : byteValue pairs:'
     # print dic
     # print
 
     # read 32-bit file size (number of encoded bytes) value
-    numBytes = long(bitReader(32), 2) + 1
+    numBytes = long(bitReader(32,byteArr), 2) + 1
     print 'Number of bytes to decode:', numBytes
     
     # read the encoded data, decode it, write into the output file
@@ -146,9 +189,23 @@ elif mode == 'd': # FILE DECODING
         # read bits until a decoding match is found
         encodingBitStr = ''
         while True:
-            encodingBitStr += bitReader(1)
+            encodingBitStr += bitReader(1,byteArr)
             if encodingBitStr in dic:
                 byteValue = dic[encodingBitStr]
                 fo.write(chr(byteValue))
                 break
     fo.close()
+
+    bitPosition = 0
+
+
+if __name__ == "__main__":
+    filename = raw_input("enter filename")
+    print 'to compress:1 ; to decompress:2'
+    choice = int(input())
+    if choice==1:
+        shannon_compress()
+    elif choice==2:
+        shannon_decompress()
+    else:
+    	print "wrong input"
